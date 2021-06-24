ca447295141b0a41c37603ade4817657b96ab3c4
shalini <shalini.v@tvsnext.io>
2021-06-24 10:10:21 +0530

    program

---
+++ first program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace first_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
